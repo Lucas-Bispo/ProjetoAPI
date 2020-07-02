c393f197116e2ab1c1e70e81b5e26ebba646d8fc
Lucas-Bispo <lukaobispo@gmail.com>
2020-07-02 20:53:22 -0300

    edição de código

--- bin/Projeto1.dart
+++ bin/Projeto1.dart
@@ -29,6 +29,28 @@ today() async{
 //espera a resposta de algo
 //no caso desse projeto espera a resposta do site que usaremso no projeto
 Future getData() async{
-  String url = 'https://api.hgnbrasil.com/finance';
+  String url = 'https://api.hgbrasil.com/finance?key=';
+  http.Response response = await http.get(url);
 
+  if(response.statusCode == 200){
+    var data = json.decode(response.body)['results']['currencies'];
+    var usd = data['USD'];
+    var eur = data['EUR'];
+    var gbp = data['GBP'];
+    var ars = data['ARS'];
+    var btc = data['BTC'];
+
+    Map formateMap = Map();
+    formateMap['date'] = '25/01/2019'; //now();
+    formateMap['date'] = '${usd['name']}: ${usd['buy']} | ${eur['name']}: ${eur['buy']} | ${gbp['name']}: ${gbp['buy']} | ${ars['name']}: ${ars['buy']} | ${btc['name']}: ${btc['buy']}';
+    
+    return formateMap;
+  }
+  else
+   throw('Falhou!');
+}
+String now(){
+
+  var now = DateTime.now();
+  return '${now.day.toString().padLeft(2 , '0')}/ ${now.month.toString().padLeft(2 , '0')}/ ${now.year.toString().padLeft(2 , '0')}';
 }
