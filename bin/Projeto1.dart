82941918421ebaf5b9f1110aaedbe7b4c425e7e5
Lucas-Bispo <lukaobispo@gmail.com>
2020-07-03 07:30:52 -0300

    comentado algoritimo, falta a chave de api

--- bin/Projeto1.dart
+++ bin/Projeto1.dart
@@ -2,7 +2,7 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'dart:io';
 void main() {
-menu();
+menu();// Chamada de função 
 }
 
 void menu(){
@@ -10,40 +10,41 @@ void menu(){
   print('################# Início #################');
   print('\n Selecione uma das opções abaixo:');
   print('1 - Ver a cotação de hoje');
-  print('2 - Registrar a cotação de hoje');
+  print('2 - Registrar a cotação de hoje');//
 
 
-  String option = stdin.readLineSync();
+  String option = stdin.readLineSync(); //os dados do teclado como string
 
-  switch(int.parse(option)){
+  switch(int.parse(option)){//parse converte para interiro
     case 1: today(); break;
     case 2: registerData(); break;
     default : print('\n\nOps, opção inválida. Selecione uma opção válida!\n\n'); menu(); break;
+    //caso dê erro chama o menu de novo
   }
 
 }
-
-registerData() async{
-  var hgData = await getData();
-  dynamic fileData = readFile();
+registerData() async{//pegar dados e colocar em um arquivo
+  var hgData = await getData(); //consultar api e jogar os dados em variavel
+  dynamic fileData = readFile();//
 
   fileData = (fileData != null && fileData.length > 0 ? json.decode(fileData) : List());
+//condição de verificação de arquivo referente aos dados que foram solicitados, se tiver vazio retorna uma lista 
 
   bool exists = false;
 
   fileData.forEach((data) {
     if (data['date'] == now())
       exists = true;  
-  });
+  });//teste que verifica informação de hoje, se tiver não registra 
 
-  if(!exists){
+  if(!exists){//se não exitir adiciona 
     fileData.add({"date": now(), "data": "${hgData['data']}"});
 
-    Directory dir = Directory.current;
-    File file = new File(dir.path + '/meu_artquivo.txt');
-    RandomAccessFile raf = file.openSync(mode: FileMode.write);
+    Directory dir = Directory.current;//criação de arquivo 
+    File file = new File(dir.path + '/meu_artquivo.txt');//carregar arquivo
+    RandomAccessFile raf = file.openSync(mode: FileMode.write);//abrir arquivo em modo de escrita
 
-    raf.writeStringSync(json.encode(fileData).toString());
+    raf.writeStringSync(json.encode(fileData).toString());//conversão de arquivo
     raf.flushSync();
     raf.closeSync();
 
@@ -55,20 +56,21 @@ registerData() async{
 
 }
 
-today() async{
-  var data = await getData();
-  print('##################################### Hg Brasil - Coleção #####################################');
+today() async{//função pega cotação, 
+  var data = await getData();//retorna objeto futuro usasse async
+  print('##################################### Hg Brasil - Cotação #####################################');
   print('${data['date']}-> ${data[data]}');
 
 }
 //espera a resposta de algo
 //no caso desse projeto espera a resposta do site que usaremso no projeto
-Future getData() async{
-  String url = 'https://api.hgbrasil.com/finance';
-  http.Response response = await http.get(url);
+Future getData() async{ // 
+  String url = 'https://api.hgbrasil.com/finance'; //api financeira
+  http.Response response = await http.get(url);//espera de requisição de sucesso código
+  //de sucesso é o 200
 
   if(response.statusCode == 200){
-    var data = json.decode(response.body)['results']['currencies'];
+    var data = json.decode(response.body)['results']['currencies'];//chamada de função do dart convert
     var usd = data['USD'];
     var eur = data['EUR'];
     var gbp = data['GBP'];
@@ -76,7 +78,7 @@ Future getData() async{
     var btc = data['BTC'];
 
     Map formateMap = Map();
-    formateMap['date'] = '25/01/2019'; //now();
+    formateMap['date'] = now();
     formateMap['date'] = '${usd['name']}: ${usd['buy']} | ${eur['name']}: ${eur['buy']} | ${gbp['name']}: ${gbp['buy']} | ${ars['name']}: ${ars['buy']} | ${btc['name']}: ${btc['buy']}';
     
     return formateMap;
@@ -86,11 +88,11 @@ Future getData() async{
 }
 String now(){
 
-  var now = DateTime.now();
+  var now = DateTime.now();//a função a baixo testa se um caracter ou dois
   return '${now.day.toString().padLeft(2 , '0')}/ ${now.month.toString().padLeft(2 , '0')}/ ${now.year.toString().padLeft(2 , '0')}';
 }
 
-String readFile(){
+String readFile(){//buscar dados no arquivos 
   Directory dir = Directory.current;
   File file = new File(dir.path + '/meu_artquivo.txt');
 
